03762ad7c9ec070541fbe8aae8cc1501010f5ee1
TsukasaMita <tsukasa.mita@gmail.com>
2026-03-04 10:22:15 +0100

    Implementiere CarManager-Klasse und integriere Fahrzeugerstellung und -bewegung in das Hauptspiel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Screen
+import car_manager
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -8,16 +9,18 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.move, "Up")
-
-
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
 
 
     
