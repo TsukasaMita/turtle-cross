23183b7b5d13d37dac1e1c8b0f2a4164546f3c49
TsukasaMita <tsukasa.mita@gmail.com>
2026-03-09 12:41:26 +0100

    Implementiere Scoreboard-Klasse zur Anzeige des Spielstands und der Level

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.move, "Up")
@@ -26,11 +27,13 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     # Detect successful crossing
     if player.ist_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        
 
 
 screen.exitonclick()
